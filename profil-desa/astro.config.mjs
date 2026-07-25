cf281dc3d558e649f7db3d0f353cba7a40dffee3
RyianDzuhri <muhammadryiandzuhri@gmail.com>
2026-07-25 11:42:48 +0800

    setting konfigurasi

--- profil-desa/astro.config.mjs
+++ profil-desa/astro.config.mjs
@@ -1,3 +1,13 @@
+// // @ts-check
+// import { defineConfig } from 'astro/config';
+// import tailwindcss from '@tailwindcss/vite';
+
+// export default defineConfig({
+//   vite: {
+//     plugins: [tailwindcss()]
+//   }
+// });
+
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
