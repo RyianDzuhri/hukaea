7bd9a0519dd9b75793232e8534648afadb19ed81
RyianDzuhri <muhammadryiandzuhri@gmail.com>
2026-07-25 12:21:23 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,8 @@
-// // @ts-check
-// import { defineConfig } from 'astro/config';
-// import tailwindcss from '@tailwindcss/vite';
-
-// export default defineConfig({
-//   vite: {
-//     plugins: [tailwindcss()]
-//   }
-// });
-
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'http://desahukaea.web.id/',
-  base: '/hukaea',
+  site: 'https://desahukaea.web.id',
 
   vite: {
     plugins: [tailwindcss()]
